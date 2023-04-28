847484ff1da4adb393d80ad55c6864283d36ef34
HakanKara75 <hakanbatirhan@gmail.com>
2023-04-28 12:42:19 +0300

    v

---
+++ src/test/java/p1/C03_Trendyol_Uyelik_Bilgileri_Girisi_Select.java
@@ -0,0 +1,131 @@
+package p1;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import utilities.TestBase;
+import com.github.javafaker.Faker;
+
+
+
+public class C03_Trendyol_Uyelik_Bilgileri_Girisi_Select extends TestBase {
+    @Test
+    public void name() {
+        extentTest = extentReports.createTest("ExtentTest", "Trendyol Uyelik Test Raporu");
+        //   "https://www.trendyol.com/" sitesine git
+        driver.get("https://www.trendyol.com/");
+        extentTest.info("Trendyol sayfasına gidildi");
+        try {
+            driver.findElement(By.id("Rating-Review")).click();
+            driver.findElement(By.id("rejectAllButton")).click();
+        } catch (Exception e) {
+
+        }
+
+        //Giris menusu ustune git
+        moveToElement("//p[text()='Giriş Yap']");
+        extentTest.info("Giris menusu ustune gidildi ");
+
+        //menunun acildigini dogrula
+        WebElement menu = findXpathWebelement("//div[@class='login-button']");
+        assertTrueIsDisplayed(menu);
+        menu.click();
+        extentTest.info("menunun acildigi dogrulandi");
+
+        //uyelik bilgilerini gir
+        driver.findElement(By.xpath("//input[@id='login-email']")).sendKeys("hakanbatirhan@gmail.com");
+        webElementSendKeys("//input[@id='login-password-input']", ",bbbbbbb");
+        threadSleep(3);
+        findByXpathClick("//button[@class='q-primary q-fluid q-button-medium q-button submit']");
+        extentTest.info("Uyelik bilgileri girildi");
+
+        //supermarket menusu ustune git
+        threadSleep(2);
+        moveToElement("//a[@href='/butik/liste/16/supermarket']");
+        extentTest.info("supermarket menusu ustune gidildi");
+
+        //kedi mamasini tikla
+        findByXpathClick("//a[@href='/kedi-mamasi-x-c103588']");
+        extentTest.info("kedi mamasi tiklandi");
+
+        //altinci urunden 5 tane sepete ekle
+        scrollToElement("(//div[@class='add-to-bs-tx'])[6]");
+        for (int i = 1; i <6 ; i++) {
+            threadSleep(5);
+            findByXpathClick("(//div[@class='add-to-bs-tx'])[6]");
+        }
+
+        extentTest.info("altinci urunden 5 tane sepete eklendi");
+
+        //sepete tikla
+        scrollTopByJavaScript();
+        findByXpathClick("//a[@class='link account-basket']");
+        extentTest.info("sepete tiklandi");
+
+        //sepette eklenen urunden 5 tane oldugunu dogrula
+        WebElement firsProd=driver.findElement(By.xpath("//input[@value='5']"));
+        String getValue= firsProd.getAttribute("value");
+        System.out.println(getValue);
+        asserTextContainsAssertTrue("5", getValue);
+        extentTest.info("isepette eklenen urunden 5 tane oldugunu dogrulandi");
+
+        //sepeti onayla
+        findByXpathClick("(//a[@href='/sepetim/odeme'])[1]");
+
+        //yeni adres ekle
+        findByXpathClick("(//div[@class='p-add-address-box ty-p-flex-center'])[1]");
+        Faker faker=new Faker();
+        webElementSendKeys("//input[@name='name']", String.valueOf(faker.name().firstName()));
+        webElementSendKeys("//input[@name='surname']", String.valueOf(faker.name().lastName()));
+        WebElement phone=findXpathWebelement("//input[@name='phone']");
+
+        Actions actions=new Actions(driver);
+        actions.sendKeys(phone, "543321130", Keys.ENTER).perform();
+
+        //il sec
+        WebElement dropdownElement = driver.findElement(By.name("cityId"));
+        dropdownElement.click();
+        WebElement optionElement = driver.findElement(By.xpath("//div[@class='ty-select-option' and text()='Adıyaman']"));
+        optionElement.click();
+        threadSleep(3);
+
+        //ilce sec
+        WebElement ilceElement = driver.findElement(By.name("districtId"));
+        ilceElement.click();
+        WebElement optionIlce = driver.findElement(By.xpath("//div[@class='ty-select-option' and text()='Besni']"));
+        optionIlce.click();
+        threadSleep(3);
+
+        //mahalle sec
+        WebElement mahelleElement = driver.findElement(By.name("neighborhoodId"));
+        mahelleElement.click();
+        WebElement optionMahalle = driver.findElement(By.xpath("//div[@class='ty-select-option' and text()='Abımıstık Mah (Çakırhüyük Köyü)']"));
+        optionMahalle.click();
+        threadSleep(3);
+
+
+        //adres gir
+        WebElement adres=findXpathWebelement("//textarea[@name='addressLine']");
+
+        actions.sendKeys(adres, "sdfgşldjfgsdf", Keys.TAB).sendKeys("Hakan", Keys.TAB).perform();
+        findByXpathClick("//button[@type='submit']");
+
+        //Tamam butonuna bas
+        findByXpathClick("//div[@class='otp-success-button']");
+
+        //Kaydet ve Devam Et butonuna bas
+        String odemeSayfasi=driver.getWindowHandle();
+        findByXpathClick("(//button[@class='ty-primary-btn ty-btn-large'])[1]");
+
+        //bir onceki sayfaya don
+        driver.navigate().back();
+
+        //dondugun sayfanin sepetim/odeme sayfasi olmadinigini dogrula
+        String sepet=driver.getWindowHandle();
+        assertTrueEquals(odemeSayfasi,sepet);
+
+        //kapat
+}
+}
--- src/test/java/utilities/TestBase.java
+++ src/test/java/utilities/TestBase.java
@@ -1,26 +1,37 @@
 package utilities;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-
-public abstract class TestBase {
-    //TestBase class'indan obje olusturmanin onune gecilmesi icin abstract yapilabilir
+//TestBase class'indan obje olusturmanin onune gecilmesi icin abstract yapilabilir
 //Orn: TestBase base=new TestBase();
 //Bu class'a extend ettigimiz test classlarinda ulasabiliriz.
-    protected static WebDriver driver;
+
+ //Extent Report icin: Raporlamayı başlatır
+//Extent Report icinÇ Raporu HTML formatında düzenler
+//Extent Report icin: Tüm test aşamalarında extentTest objesi ile bilgi ekleriz
+
+public abstract class TestBase {
 
     @Before
     public void setUp() throws Exception {
@@ -28,10 +39,26 @@ public abstract class TestBase {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        //Extent Report icin asagisi
+        //----------------------------------------------------------------------------------------
+        extentReports = new ExtentReports();
+        String tarih = new SimpleDateFormat("_hh_mm_ss_ddMMyyyy").format(new Date());
+        String dosyaYolu = "TestOutput/reports/extentReport_" + tarih + ".html";
+        extentHtmlReporter = new ExtentHtmlReporter(dosyaYolu);
+        extentReports.attachReporter(extentHtmlReporter);
+        //Raporda gözükmesini istediğimiz bilgiler için
+        extentReports.setSystemInfo("Browser", "Chrome");
+        extentReports.setSystemInfo("Tester", "Hakan");
+        extentHtmlReporter.config().setDocumentTitle("Extent Report");
+        extentHtmlReporter.config().setReportName("Smoke Test Raporu");
+        extentTest = extentReports.createTest("ExtentTest", "Test Raporu");
+
     }
 
     @After
     public void tearDown() throws Exception {
+        extentReports.flush(); //Extent Report icin
         Thread.sleep(3000);
         driver.quit();
     }
@@ -44,65 +71,178 @@ public abstract class TestBase {
         }
     }
 
-    public WebElement findXpathWebelement(String str){
-        WebElement w= driver.findElement(By.xpath(str));
+    public WebElement findXpathWebelement(String str) {
+        WebElement w = driver.findElement(By.xpath(str));
         return w;
     }
 
-    public void asserTextContainsAssertTrue(String str, String atr){
+    public void asserTextContainsAssertTrue(String str, String atr) {
         assertTrue(str.contains(atr));
     }
 
-    public void switchAlertAccept(){
+    public void switchAlertAccept() {
         driver.switchTo().alert().accept();
     }
 
-    public void switchAlertDismiss(){
+    public void switchAlertDismiss() {
         driver.switchTo().alert().dismiss();
     }
 
-    public void switchAlertSendKey(String str){
+    public void switchAlertSendKey(String str) {
         driver.switchTo().alert().sendKeys(str);
     }
 
-    public String findByXpathString(String str){
-        String location= driver.findElement(By.xpath(str)).getText();
+    public String findByXpathString(String str) {
+        String location = driver.findElement(By.xpath(str)).getText();
         return location;
     }
 
-    public void findByXpathClick(String str){
+    public void findByXpathClick(String str) {
         driver.findElement(By.xpath(str)).click();
     }
 
-    public void pageDown(){
-        Actions actions=new Actions(driver);
+    public void findByIdClick(String str) {
+        driver.findElement(By.id(str)).click();
+    }
+
+    public WebElement findByIdWebelement(String str) {
+        WebElement w = driver.findElement(By.id(str));
+        return w;
+    }
+
+    public void pageDown() {
+        Actions actions = new Actions(driver);
         actions.sendKeys(Keys.PAGE_DOWN).perform();
     }
 
-    public void pageUp(){
-        Actions actions=new Actions(driver);
+    public void pageUp() {
+        Actions actions = new Actions(driver);
         actions.sendKeys(Keys.PAGE_UP).perform();
     }
 
-    public void arrowDown(){
-        Actions actions=new Actions(driver);
+    public void arrowDown() {
+        Actions actions = new Actions(driver);
         actions.sendKeys(Keys.ARROW_DOWN).perform();
     }
 
-    public void arrowUp(){
-        Actions actions=new Actions(driver);
+    public void arrowUp() {
+        Actions actions = new Actions(driver);
         actions.sendKeys(Keys.ARROW_UP).perform();
     }
 
-    public void assertDisplayedWebelement(WebElement a){
+    public void assertDisplayedWebelement(WebElement a) {
         assertTrue(a.isDisplayed());
     }
 
-    public void scrollToElement(String str){
+    public void scrollToElement(String str) {
         WebElement bottom = driver.findElement(By.xpath(str));
-        Actions actions=new Actions(driver);
+        Actions actions = new Actions(driver);
         actions.scrollToElement(bottom).perform();
         //bu kod locati alinan elemana kadar sayfayi asagi goturur
     }
 
+    public static void tumSayfaScreenShoot() {
+        String tarih = new SimpleDateFormat("_hh_mm_ss_ddMMyyyy").format(new Date());
+        String dosyaYolu = "TestOutput/screenshot" + tarih + ".png";
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        try {
+            FileUtils.copyFile(ts.getScreenshotAs(OutputType.FILE), new File(dosyaYolu));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //WebElement ScreenShot
+    public static void webElementScreenShoot(WebElement element) {
+        String tarih = new SimpleDateFormat("_hh_mm_ss_ddMMyyyy").format(new Date());
+        String dosyaYolu = "TestOutput/webElementScreenshot" + tarih + ".png";
+        try {
+            FileUtils.copyFile(element.getScreenshotAs(OutputType.FILE), new File(dosyaYolu));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void switchToWindow(int sayi) {
+        List<String> tumWindowHandles = new ArrayList<String>(driver.getWindowHandles());
+        driver.switchTo().window(tumWindowHandles.get(sayi));
+    }
+
+    //SwitchToWindow2
+    public static void window(int sayi) {
+        driver.switchTo().window(driver.getWindowHandles().toArray()[sayi].toString());
+    }
+    //bu metot ile herhangi bir webelemente JavascriptExecutor kullanarak tiklayabilirim
+    public void clickByJavaScript(WebElement webElement){
+        JavascriptExecutor jse= (JavascriptExecutor) driver;
+
+        jse.executeScript("arguments[0].click();", webElement);
+
+    }
+
+    public void scrollIntoViewByJavaScript(WebElement webElement){
+        JavascriptExecutor jse=(JavascriptExecutor) driver;//Casting
+        jse.executeScript("arguments[0].scrollIntoView(true);",webElement);
+
+    }
+
+    //bu metot ile sayfayi en alta kaydirabilirim
+    public void scrollEndByJavaScript(){
+        JavascriptExecutor js= (JavascriptExecutor) driver;
+        js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
+    }
+
+    //bu metot ile sayfayi en yukari kaydirabilirim
+    public void scrollTopByJavaScript(){
+        JavascriptExecutor js= (JavascriptExecutor) driver;
+        js.executeScript("window.scrollTo(0, -document.body.scrollHeight);");
+    }
+
+    public void typeWithJavaScript(WebElement webElement, String str){
+        JavascriptExecutor js= (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].setAttribute('value', '"+str+"')", webElement);
+    }
+
+    //bu metot ile attribute degerleri ile texti alabilirim
+    public String getValueByJavaScript(String id, String attributeName){
+        JavascriptExecutor js= (JavascriptExecutor) driver;
+        String string= js.executeScript("return document.getElementById('"+id+"')."+attributeName+"").toString();
+        System.out.println(string);
+        return string;
+        //        NOT: document.querySelector("p").value;  -> TAG KULLANILABILIR
+//             document.querySelector(".example").value; -> CSS DEGERI KULLANILABILIR
+//             document.querySelector("#example").value; -> CSS DEGERI KULLANILABILIR
+    }
+    public void moveToElement(String str){
+        WebElement webElement = driver.findElement(By.xpath(str));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(webElement).perform();
+
+
+    }
+    public void assertTrueIsDisplayed(WebElement webElement){
+        Assert.assertTrue(webElement.isDisplayed());
+    }
+
+    public void assertTrueEquals(String str, String str1){
+        Assert.assertTrue(str.equals(str1));
+    }
+    public void assertTrueIsSelected(WebElement webElement){
+        Assert.assertTrue(webElement.isSelected());
+    }
+
+    public void assertTrueIsEnabled(WebElement webElement){
+        Assert.assertTrue(webElement.isEnabled());
+    }
+
+    public void webElementSendKeys(String xPath, String sendKeys){
+        driver.findElement(By.xpath(xPath)).sendKeys(sendKeys);
+
+    }
+
+    protected static WebDriver driver;
+    protected static ExtentReports extentReports;
+    protected static ExtentHtmlReporter extentHtmlReporter;
+    protected static ExtentTest extentTest;
 }
+
