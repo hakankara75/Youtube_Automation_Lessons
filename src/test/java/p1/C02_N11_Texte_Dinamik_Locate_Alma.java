5fc4233d856145c0bd51f966e6daba7669cab4fb
HakanKara75 <hakanbatirhan@gmail.com>
2023-05-02 22:39:16 +0300

    v

---
+++ src/test/java/p1/C02_N11_Texte_Dinamik_Locate_Alma.java
@@ -0,0 +1,19 @@
+package p1;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import utilities.TestBase;
+
+public class C02_N11_Texte_Dinamik_Locate_Alma  extends TestBase {
+    @Test
+    public void test() {
+        driver.get("https://www.n11.com");
+        WebElement aramaKutusu= driver.findElement(By.xpath("//input[@id='searchData']"));
+        aramaKutusu.sendKeys("Fındık Kreması", Keys.ENTER);
+        int sonuc= driver.findElement()
+
+
+    }
+}
--- src/test/java/p1/C02_Trendyol.java
+++ src/test/java/p1/C02_Trendyol.java
@@ -6,9 +6,11 @@ import utilities.TestBase;
 
 public class C02_Trendyol extends TestBase {
     @Test
-    public void name() {
+    public void name() throws InterruptedException {
         driver.get("https://www.trendyol.com/uyelik?cb=https%3A%2F%2Fwww.trendyol.com%2F");
 driver.findElement(By.xpath("(//div[@class='ty-mgr-2 ty-relative ty-checkbox-container'])[2]")).click();
+Thread.sleep(3000);
+
 
 
 
