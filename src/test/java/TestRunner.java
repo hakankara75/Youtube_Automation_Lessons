30e925861258b9301f44124f7b6fe4af20ef1626
HakanKara75 <hakanbatirhan@gmail.com>
2023-09-07 22:29:54 +0300

    v

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
                 "json:target/json-reports/cucumber.json",
                 "junit:src/test/resources/features/htmlReport/cucumber.xml",
                 "rerun:TestOutput/failed_scenario.txt",//rerun hata veren testleri kaydedip yeniden otomatik kosmak icin
-                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+               // "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
         },
         features = "src/test/resources/features",
         glue = "stepDefinitions",
