845880a60b2398022b9c55a0259d312c724c1d96
HakanKara75 <hakanbatirhan@gmail.com>
2023-09-13 11:38:40 +0300

    v

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "stepDefinitions",
-        tags = "@kitap",
+        tags = "@nutella",
         dryRun = false
 
 )
