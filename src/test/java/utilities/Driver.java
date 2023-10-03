5a0d25b7bdf22b1c8c131703cccce73a223ab64d
HakanKara75 <hakanbatirhan@gmail.com>
2023-10-03 17:56:56 +0300

    v

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -5,45 +5,36 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-
-import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class Driver {
     /*
-       Driver class'ındaki temel mantık extends yöntemiyle değil yani TestBase class'ına extent etmek yerine
-   Driver class'ından static methodlar kullanarak driver oluştururuz. Static olduğu için class ismi ile
-   her yerden methoda ulaşabileceğiz.
-    */
+        Driver class'ındaki temel mantık extends yöntemiyle değil yani ReusableMethods class'ına extent etmek yerine
+    Driver class'ından static methodlar kullanarak driver oluştururuz. Static olduğu için class ismi ile
+    her yerden methoda ulaşabileceğiz.
+     */
     /*
-        Singleton Pattern: Tekli kullanım kalıbı.
-            Bir class'tan obje oluşturulmasının önüne geçilmesi için kullanılan ifade
-            Bir class'tan obje oluşturmanın önüne geçmek için default constructor'ın kullanımını engellemek için
-        private access modifire kullanarak bir constructor oluştururuz
-         */
-    private Driver() {
+    Singleton Pattern: Tekli kullanım kalıbı.
+        Bir class'tan obje oluşturulmasının önüne geçilmesi için kullanılan ifade
+        Bir class'tan obje oluşturmanın önüne geçmek için default constructor'ın kullanımını engellemek için
+    private access modifire kullanarak bir constructor oluştururuz
+     */
+    private Driver(){
 
     }
-
-    private static WebDriver driver;
+    static WebDriver driver;
 
     public static WebDriver getDriver() {
-         /*
+        /*
             Driver'i her çağırdığında yeni bir pencere açılmasının önüne geçmek için
         if bloğu içinde Eğer driver'a değer atanmamışsa(driver doluysa) değer ata, Eğer değer atanmışsa Driver'i aynı
         sayfada RETURN et. Bunun sadece yapmamız gereken if(driver==null) kullanmak
          */
         if (driver == null) {
-
             switch (ConfigReader.getProperty("browser")) {
                 case "chrome":
 
                     driver = new ChromeDriver(new ChromeOptions().addArguments("--remote-allow-origins=*"));
-                    break;
-                case "firefox":
-
-                    driver = new FirefoxDriver(new FirefoxOptions().addArguments("--remote-allow-origins=*"));
                     break;
                 case "edge":
 
@@ -56,22 +47,28 @@ public class Driver {
                     break;
                 default:
 
-                    driver = new EdgeDriver();
-
-
+                    driver = new ChromeDriver(new ChromeOptions().addArguments("--remote-allow-origins=*"));
             }
 
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         }
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-        driver.manage().window().maximize();
+
         return driver;
     }
 
     public static void closeDriver() {
-        if (driver != null) {//if driver is pointing anywhere
-            driver.quit();//quit when I call closeDriver
-            driver = null;//make the driver null so when we call getDriver, we can open the driver again
+        if (driver != null) {//Driver'a değer atanmışşsa
+            driver.close();
+            driver = null;
         }
     }
 
+    public static void quitDriver() {
+        if (driver != null) {//Driver'a değer atanmışşsa
+            driver.quit();
+            driver = null;
+        }
+    }
 }
+
