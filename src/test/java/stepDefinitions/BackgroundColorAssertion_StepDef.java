2477b4f6e73de1ea8691b413a3e7a61cdd73e257
hakankara75 <hakanbatirhan@gmail.com>
2024-02-02 15:22:34 +0300

    v

--- src/test/java/stepDefinitions/BackgroundColorAssertion_StepDef.java
+++ src/test/java/stepDefinitions/BackgroundColorAssertion_StepDef.java
@@ -10,7 +10,7 @@ import static org.junit.Assert.assertEquals;
 import static utilities.Driver.getDriver;
 
 public class BackgroundColorAssertion_StepDef {
-    BackgroundColorAssertion_Page page= new BackgroundColorAssertion_Page();
+    BackgroundColorAssertion_Page page = new BackgroundColorAssertion_Page();
 
     @Given("kullanici Trendyol sitesine gider")
     public void kullaniciTrendyolSitesineGider() {
@@ -19,15 +19,16 @@ public class BackgroundColorAssertion_StepDef {
 
     @Then("kullanici cok satanlar linki yanindaki yeni butonunun rengini dogrular")
     public void kullaniciCokSatanlarLinkiYanindakiYeniButonununRenginiDogrular() {
-            String expectedRgbColorCode= "#dc2e2e";
+
+        String expectedRgbColorCode = "#dc2e2e";
         System.out.println("expectedRgbColorCode = " + expectedRgbColorCode);
 
-        String backgroundColor= page.yeni.getCssValue("background-color");
+        String backgroundColor = page.yeni.getCssValue("background-color");
         System.out.println("backgroundColor = " + backgroundColor);
 
-        Color color= Color.fromString(backgroundColor);
+        Color color = Color.fromString(backgroundColor);
         System.out.println("color = " + color);
-        String actualRgbColorCode= color.asHex();
+        String actualRgbColorCode = color.asHex();
         System.out.println("actualRgbColorCode = " + actualRgbColorCode);
 
         assertEquals(expectedRgbColorCode, actualRgbColorCode);
